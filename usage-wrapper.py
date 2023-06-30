e48f25e896c1a3242d099fa7548ff06f0df22fac
John <1@1.com>
2023-06-30 11:30:16 -0400

    resynching, added more input sanitization to tags

--- usage-wrapper.py
+++ usage-wrapper.py
@@ -12,4 +12,4 @@ parsed = parsed.parse_args()
 
 jbx = jbxapi.JoeSandbox(apikey=parsed.apikey)
 with open(parsed.filepath, 'rb') as file:
- print(json.dumps(jbx.submit_sample(file, _extra_params={"comments":parsed.comment, "analysis-time":parsed.time, "tags[]":parsed.tags.split(',')})))
+ print(json.dumps(jbx.submit_sample(file, _extra_params={"comments":parsed.comment, "analysis-time":parsed.time, "tags[]":parsed.tags.strip().rstrip(",").strip().split(',')})))
